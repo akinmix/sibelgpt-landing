23f3f6aa6840c7cc2578bf975ba7141c9e069cab
akinmix <akinmidi@gmail.com>
2025-05-02 11:34:45 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -460,7 +460,7 @@ window.addEventListener('beforeunload', () => {
 });
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://qkjyysjbtfxwyyypuhzs.supabase.co;
+const supabaseUrl = 'https://qkjyysjbtfxwyyypuhzs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFranl5c2pidGZ4d3l5eXB1aHpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MzE5MjYsImV4cCI6MjA2MTQwNzkyNn0.k1GvvvoYYqXKPJzx27wBB5ncqPHqnObW_b67spw4c1E';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -485,4 +485,4 @@ supabase.auth.getUser().then(({ data: { user } }) => {
     sidebar.appendChild(welcome);
   }
 });
-"
+
